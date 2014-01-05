9d3c9806d31255dbb032f93e8bb2b6cb4eb9391d
Chris Moore <cdmwebs@gmail.com>
2014-01-05 12:25:21 -0500

    I guess it works

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,7 @@
 var CincyService = window.CincyService = Ember.Application.create();
 
+$(document).foundation();
+
 /* Order and include as you please. */
 require('scripts/controllers/*');
 require('scripts/store');
