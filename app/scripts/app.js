7499aba2397f74f6385ad4f4e94b832dc9a6c87e
Chris Moore <cdmwebs@gmail.com>
2014-01-05 12:39:52 -0500

    Foundation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,5 @@
 var CincyService = window.CincyService = Ember.Application.create();
 
-$(document).foundation();
-
 /* Order and include as you please. */
 require('scripts/controllers/*');
 require('scripts/store');
@@ -9,3 +7,8 @@ require('scripts/models/*');
 require('scripts/routes/*');
 require('scripts/views/*');
 require('scripts/router');
+
+$(document).ready(function() {
+  $(document).foundation();
+});
+
